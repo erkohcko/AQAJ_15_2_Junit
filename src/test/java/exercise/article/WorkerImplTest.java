a1fdfa65f04b26dbcac8e9f3c7eb4c15c1b2de07
Erkochko <wenarun@yandex.ru>
2023-07-08 05:54:46 +0300

    код стайл правка

--- src/test/java/exercise/article/WorkerImplTest.java
+++ src/test/java/exercise/article/WorkerImplTest.java
@@ -31,7 +31,7 @@ public class WorkerImplTest {
 
     @Test
     public void addNewArticlesCallsLibraryStore() {
-        List<Article> art = new ArrayList<>();
+        List < Article > art = new ArrayList < > ();
         art.add(new Article("Статья 1", "Содержимое 1", "Автор 1", LocalDate.of(2023, 7, 10)));
         art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 7, 10)));
         worker.addNewArticles(art);
@@ -40,47 +40,47 @@ public class WorkerImplTest {
 
     @Test
     public void getCatalogReturnsNonEmptyStringWhenLibraryHasNoArticles() {
-        when(library.getAllTitles()).thenReturn(new ArrayList<>());
+        when(library.getAllTitles()).thenReturn(new ArrayList < > ());
         String catalog = worker.getCatalog();
         assertFalse(catalog.isEmpty());
     }
 
     @Test
     public void getCatalogReturnsStringWithArticleTitlesWhenLibraryHasArticles() {
-        List<String> titles = List.of("Статья 1", "Статья 2", "Статья 3");
+        List < String > titles = List.of("Статья 1", "Статья 2", "Статья 3");
         when(library.getAllTitles()).thenReturn(titles);
         String catalog = worker.getCatalog();
-        for (String title : titles) {
+        for (String title: titles) {
             assertEquals(true, catalog.contains(title));
         }
     }
 
     @Test
     public void prepareArticlesRemovesArticlesWithoutTitle() {
-        List<Article> art = new ArrayList<>();
+        List < Article > art = new ArrayList < > ();
         art.add(new Article(null, "Содержимое 1", "Автор 1", LocalDate.of(2023, 7, 10)));
         art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 7, 10)));
-        List<Article> preparedArticles = worker.prepareArticles(art);
+        List < Article > preparedArticles = worker.prepareArticles(art);
         assertEquals(1, preparedArticles.size());
         assertEquals("Статья 2", preparedArticles.get(0).getTitle());
     }
 
     @Test
     public void prepareArticlesRemovesArticlesWithoutContent() {
-        List<Article> art = new ArrayList<>();
+        List < Article > art = new ArrayList < > ();
         art.add(new Article("Статья 1", null, "Автор 1", LocalDate.of(2023, 7, 10)));
         art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 7, 10)));
-        List<Article> preparedArticles = worker.prepareArticles(art);
+        List < Article > preparedArticles = worker.prepareArticles(art);
         assertEquals(1, preparedArticles.size());
         assertEquals("Статья 2", preparedArticles.get(0).getTitle());
     }
 
     @Test
     public void prepareArticlesSetsDefaultAuthor() {
-        List<Article> art = new ArrayList<>();
+        List < Article > art = new ArrayList < > ();
         art.add(new Article("Статья 1", "Содержимое 1", "Автор 1", LocalDate.of(2023, 7, 10)));
         art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 7, 10)));
-        List<Article> preparedArticles = worker.prepareArticles(art);
+        List < Article > preparedArticles = worker.prepareArticles(art);
         assertEquals(2, preparedArticles.size());
         assertEquals("Автор 1", preparedArticles.get(0).getAuthor());
         assertEquals("Автор 2", preparedArticles.get(1).getAuthor());
@@ -88,10 +88,10 @@ public class WorkerImplTest {
 
     @Test
     public void prepareArticlesSetsDefaultCreationDateWhenNotSpecified() {
-        List<Article> art = new ArrayList<>();
+        List < Article > art = new ArrayList < > ();
         art.add(new Article("Статья 1", "Содержимое 1", "Автор 1", null));
         art.add(new Article("Статья 2", "Содержимое 2", "Автор 2", LocalDate.of(2023, 7, 10)));
-        List<Article> preparedArticles = worker.prepareArticles(art);
+        List < Article > preparedArticles = worker.prepareArticles(art);
         assertEquals(2, preparedArticles.size());
         assertEquals(LocalDate.now(), preparedArticles.get(0).getCreationDate());
         assertEquals(LocalDate.of(2023, 7, 10), preparedArticles.get(1).getCreationDate());
@@ -99,7 +99,7 @@ public class WorkerImplTest {
     @Test
 
     void checkSameArticle() {
-        List<Article> art = new ArrayList<>();
+        List < Article > art = new ArrayList < > ();
         art.add(new Article("Книга 1", "Содержимое 1, ",
                 "Автор 1", LocalDate.of(2023, 7, 10)));
         art.add(new Article("Книга 1", "Содержимое 1, ",
